a8492d36c0ca9661d9cd9e1c9e9d8e8cd8f7c3db
Kamal-Abdullayev <kamalabdu2layev@gmail.com>
2025-01-15 19:10:53 +0200

    ref: jdbc-sink-connector

--- consumer/src/main/resources/schema.sql
+++ consumer/src/main/resources/schema.sql
@@ -1,28 +1,3 @@
-CREATE TABLE wikimedia_meta_record (
-                                       wikimedia_meta_record_id BIGSERIAL PRIMARY KEY,
-                                       uri VARCHAR(255),
-                                       request_id VARCHAR(255),
-                                       id VARCHAR(255),
-                                       meta_update_time TIMESTAMP,
-                                       domain VARCHAR(255),
-                                       stream VARCHAR(255),
-                                       topic VARCHAR(255),
-                                       "partition" INT,
-                                       "offset" BIGINT
-);
-
-CREATE TABLE length (
-                        id BIGSERIAL PRIMARY KEY,
-                        old_length BIGINT,
-                        new_length BIGINT
-);
-
-CREATE TABLE revision (
-                          id BIGSERIAL PRIMARY KEY,
-                          old_revision BIGINT,
-                          new_revision BIGINT
-);
-
 CREATE TABLE wikimedia_record (
                                   wikimedia_record_id BIGSERIAL PRIMARY KEY,
                                   id BIGINT,
@@ -42,21 +17,15 @@ CREATE TABLE wikimedia_record (
                                   wiki VARCHAR(255),
                                   parsedcomment TEXT,
                                   meta_id VARCHAR(255),
-                                  length_id BIGINT,
-                                  revision_id BIGINT
+                                  namespace INT DEFAULT 0,
+                                  meta_uri VARCHAR(255) DEFAULT '',
+                                  meta_partition INT DEFAULT 0,
+                                  meta_offset BIGINT DEFAULT 0,
+                                  meta_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                  meta_domain VARCHAR(255) DEFAULT '',
+                                  meta_stream VARCHAR(255) DEFAULT '',
+                                  meta_topic VARCHAR(255) DEFAULT 'default_value',
+                                  meta_dt VARCHAR(255) DEFAULT ''
 );
 
-ALTER TABLE wikimedia_record ADD COLUMN meta JSONB;
-ALTER TABLE wikimedia_record ADD COLUMN namespace INT DEFAULT 0;
-ALTER TABLE wikimedia_record ADD COLUMN length JSONB;
-ALTER TABLE wikimedia_record ADD COLUMN revision JSONB;
-ALTER TABLE wikimedia_record ADD COLUMN old_length BIGINT;
-ALTER TABLE wikimedia_record ADD COLUMN new_length BIGINT;
-ALTER TABLE wikimedia_record ADD COLUMN meta_partition INT DEFAULT 0;
-ALTER TABLE wikimedia_record ADD COLUMN meta_uri VARCHAR(255) DEFAULT '';
-ALTER TABLE wikimedia_record ADD COLUMN meta_offset BIGINT DEFAULT 0;
-ALTER TABLE wikimedia_record ADD COLUMN meta_domain VARCHAR(255) DEFAULT '';
-ALTER TABLE wikimedia_record ADD COLUMN meta_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE wikimedia_record ADD COLUMN meta_stream VARCHAR(255) DEFAULT '';
-ALTER TABLE wikimedia_record ADD COLUMN meta_dt VARCHAR(255) DEFAULT '';
-ALTER TABLE wikimedia_record ADD COLUMN meta_topic VARCHAR(255) DEFAULT 'default_value';
+
